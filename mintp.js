6bf0bdc303e591e7296a4aa7b2204a2db8b263ab
Ricardo Liang <ricardoliang@gmail.com>
2015-02-04 03:08:47 -0200

    now supports nesting.

--- mintp.js
+++ mintp.js
@@ -1,15 +1,14 @@
 (function() {
   'use strict';
-  var cache = {};
-  this.mintp = function(sel, args, opt_sep) {
-    return (cache[sel] ? cache[sel] : cache[sel] =
-        document.querySelector(sel).innerHTML.split(opt_sep || /[\{\}]{2}/))
-      .map(function(exp, i) {
-        return i % 2 === 0 ? exp : Function.apply(null,
-            Object.keys(args).concat('return eval(this.x);'))
-          .apply({x: exp}, Object.keys(args).map(function(k) {
-            return args[k];
-          }));
-      }).join('');
+  this.mintp = function(sel, args, opt_regex) {
+    return (function doit(text) {
+      var did = false;
+      text = text.replace(opt_regex || /\{\{((.(?!\{\{))*?)\}\}/g, function(_, exp) {
+        did = true;
+        return Function.apply(null, Object.keys(args).concat('return eval(this.x);'))
+        .apply({x: exp}, Object.keys(args).map(function(k) { return args[k]; }));
+      });
+      return did ? doit(text) : text;
+    })(document.querySelector(sel).innerHTML);
   };
 }).call(this);
